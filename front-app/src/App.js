eb8b4cfa1a876717883082cd6e428f2a27b0817c
sya224 <sya224@g.uky.edu>
2020-05-13 18:31:55 +0900

    S02P31A305-36 | DONE | Add folders [actinos, apis, components, redcers]

---
+++ front-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a>
+          발바닥
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
