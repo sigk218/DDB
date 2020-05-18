2bb9fc65b53058bc114b10d11cbd59376e915d05
MinG822 <lucida822@naver.com>
2020-05-18 23:03:13 +0900

    S02p31A305-85|DONE|modify [README.md, getHosTag.py, tagsforhos.txt]

--- data/getHosTag.py
+++ data/getHosTag.py
@@ -6,16 +6,17 @@ from krwordrank.word import summarize_with_keywords, KRWordRank
 
 komoran = Komoran()
 skipwords = [
-  '동물병원', '병원', '의사', '구름', '반려', '곳', '애견', '견', '원장', 
-  '선생님', '간호사', '직원', '스태프', '개', '강아지','고양이', '여러분',
-  '것', '거', '대체', '외', '너무', '때', '같은', '그런지', '안', '게', '모두', '년', '하루', '여자',
-  '넘', '진짜', '도담이', 
+  '동물병원', '병원', '의사', '구름', '반려', '곳', '애견', '견', '원장', '이렇다', '그렇다', '언니'
+  '선생님', '간호사', '직원', '스태프', '개', '강아지','고양이', '여러분', '스탭', '우리집'
+  '것', '거', '대체', '외', '너무', '때', '같은', '그런지', '안', '게', '모두', '년', '하루', '여자', '애기', '아가', '!!'
+  '넘', '진짜', '도담이', '사랑', '예쁘다', '이것저것', '아이', '우리', '진료', '치료', '군데', '수의사', '이참', '강지'
 ]
 umi = {
   'VA':'다', 'XR':'한', 'VV':'는', 'NNG':'', 'NN':'', 'NNP':'', 'NNB':''
 }
 
 def checkskipwords(w):
+  if len(w) == 1: return True
   for s in skipwords:
     if w in s:return True
   return False
@@ -68,11 +69,8 @@ while t < len(ko_text):
       hos_n_reviews.append(ko_text.iloc[t, 3])
   else:
     curr_hos = ko_text.iloc[t, 0]
-    a_words, n_words = [], []
-    for a in hos_a_reviews:
-      a_words += komoran_tokenize(a)
-    for n in hos_n_reviews:
-      n_words += komoran_tokenize(n)
+    a_words = komoran_tokenize('\n'.join(hos_a_reviews))
+    n_words = komoran_tokenize('\n'.join(hos_n_reviews))
     with open('tagsforhos.txt', 'a', encoding='utf-8') as file:
       if a_words:
         a_info = curr_hos+'|'+'a'+'|'+','.join(a_words)+'\n'
