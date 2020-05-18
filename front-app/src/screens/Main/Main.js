ec5162059b1fec2709e50c5207e00446c1369daf
sya224 <sya224@g.uky.edu>
2020-05-18 18:53:47 +0900

    S02P31A305-56 | In Progress | Modify [HosDetail.js, HosRes.js]

--- front-app/src/screens/Main/Main.js
+++ front-app/src/screens/Main/Main.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import MainSearchBar from "../../components/MainSearchBar/MainSearchBar";
 import imgA from "../../assets/imgA.png";
 class Main extends React.Component {
@@ -10,13 +11,25 @@ class Main extends React.Component {
 
   }
   render() {
-    console.log("bbbbb")
     return (
       <div>
         <div align="center">
           <img src={imgA} width="200" height="200"></img>
         </div>
         <MainSearchBar />
+        <div>
+          <ul>
+            <li>
+              <Link to="/MyPage">마이페이지</Link>
+            </li>
+            <li>
+              <Link to="/HosDetail">병원상세페이지</Link>
+            </li>
+            <li>
+              <Link to="/HosRes">병원 리스트</Link>
+            </li>
+          </ul>
+        </div>
       </div>
     );
   }
