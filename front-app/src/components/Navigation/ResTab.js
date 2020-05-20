c22e20fb4f933edf9796a7825ddfcf59146f9821
sya224 <sya224@g.uky.edu>
2020-05-20 18:31:00 +0900

    S02P31A305-56 | In Progress | Modify [HosDetail.js, HosRes.js, ReviewRes.js]

---
+++ front-app/src/components/Navigation/ResTab.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Paper from '@material-ui/core/Paper';
+import HosRes from '../../screens/HosRes/HosRes';
+import ReviewRes from '../../screens/ReviewRes/ReviewRes';
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`scrollable-auto-tabpanel-${index}`}
+            aria-labelledby={`scrollable-auto-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box p={3}>
+                    <Typography>{children}</Typography>
+                </Box>
+            )}
+        </div>
+    );
+}
+
+TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+    return {
+        id: `scrollable-auto-tab-${index}`,
+        'aria-controls': `scrollable-auto-tabpanel-${index}`,
+    };
+}
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        alignItems: 'center',
+        width: '100%',
+        backgroundColor: theme.palette.background.paper,
+    },
+}));
+
+export default function ResTab() {
+    const classes = useStyles();
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    return (
+        <div>
+
+            <Paper className={classes.root}>
+                <Tabs
+                    variant="fullWidth"
+                    value={value}
+                    onChange={handleChange}
+                    indicatorColor="primary"
+                    textColor="primary"
+                    scrollButtons="auto"
+                    centered
+                >
+                    <Tab label="리뷰" {...a11yProps(0)} />
+                    <Tab label="병원" {...a11yProps(1)} />
+                </Tabs>
+            </Paper>
+            <TabPanel value={value} index={0}>
+                <ReviewRes/>
+      </TabPanel>
+            <TabPanel value={value} index={1}>
+                <HosRes />
+            </TabPanel>
+        </div>
+    );
+}
