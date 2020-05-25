fecc41696317452f5867fe8da4db66d05a07d7c8
박수진 <hope772948@gmail.com>
2020-05-25 13:20:10 +0900

    S02P31A305-170 | INPROGRESS | Modify[favoriteHospitalService.java]

--- balbadack/src/main/java/com/a305/balbadack/model/service/FavoriteHospitalService.java
+++ balbadack/src/main/java/com/a305/balbadack/model/service/FavoriteHospitalService.java
@@ -1,5 +1,14 @@
 package com.a305.balbadack.model.service;
 
+import java.util.*;
+
+import com.a305.balbadack.model.dto.FavoriteHospital;
+import com.a305.balbadack.model.dto.Hospital;
+
 public interface FavoriteHospitalService {
-    
+    public void insert(FavoriteHospital favoriteHospital);
+
+    public List<Hospital> findByU_id(String u_id);
+
+    public void delete(FavoriteHospital favoriteHospital);
 }
