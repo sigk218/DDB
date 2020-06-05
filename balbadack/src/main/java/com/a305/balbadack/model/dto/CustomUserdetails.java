8895ab53e1ed37fbadabb93bfe43d042fbf78a12
박수진 <hope772948@gmail.com>
2020-06-05 13:15:21 +0900

    S02P31A305-227 | DONE | Modify[JwtService.java]

--- balbadack/src/main/java/com/a305/balbadack/model/dto/CustomUserdetails.java
+++ balbadack/src/main/java/com/a305/balbadack/model/dto/CustomUserdetails.java
@@ -11,9 +11,11 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class CustomUserdetails implements UserDetails {
 
     @JsonIgnore
